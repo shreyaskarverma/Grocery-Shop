0869664a890f30b3b9c7503146d5b99259aa308d
Shreyaskar Verma <shreyaskarverma@Shreyaskars-MacBook-Pro.local>
2023-11-11 13:06:39 +0530

    Add. Created new project.

---
+++ src/main/java/org/thoughtworks/GroceryShop.java
@@ -0,0 +1,4 @@
+package org.thoughtworks;
+
+public class GroceryShop {
+}
---
+++ src/test/java/org/thoughtworks/GroceryShopTest.java
@@ -0,0 +1,12 @@
+package org.thoughtworks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GroceryShopTest {
+
+    @Test
+    void should
+
+}
