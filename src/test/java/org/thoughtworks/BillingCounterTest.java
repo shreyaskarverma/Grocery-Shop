44178da7d98ec6e1c88cc81d4b96d79b6d24ade3
Shreyaskar Verma <shreyaskarverma@Shreyaskars-MacBook-Pro.local>
2023-11-13 01:54:11 +0530

    Add. New test methods for checking bill for multiple items added.

--- src/test/java/org/thoughtworks/BillingCounterTest.java
+++ src/test/java/org/thoughtworks/BillingCounterTest.java
@@ -54,6 +54,53 @@ class BillingCounterTest {
 
     }
 
+    @Test
+    void shouldShowBillForTwoItems()
+    {
+        BillingCounter billingCounter =  new BillingCounter();
+        ShoppingCart cart= new ShoppingCart();
+        Rate rateOfOnion= new Rate(20,WeightType.KG);
+        GroceryItem onion= new GroceryItem(rateOfOnion, GroceryItemType.ONION);
+        Quantity quantityOfOnion= new Quantity(500,WeightType.GM);
+        cart.putItem(onion,quantityOfOnion);
+        Rate rateOfApple= new Rate(200,WeightType.KG);
+        GroceryItem apple= new GroceryItem(rateOfApple, GroceryItemType.APPLE);
+        Quantity quantityOfApple= new Quantity(1,WeightType.KG);
+        cart.putItem(apple,quantityOfApple);
+
+        billingCounter.calculateBill(cart);
+
+        assertEquals(billingCounter.showBill(),210.0);
+
+
+    }
+
+    @Test
+    void shouldShowBillForThreeItems()
+    {
+        BillingCounter billingCounter =  new BillingCounter();
+        ShoppingCart cart= new ShoppingCart();
+        Rate rateOfOnion= new Rate(20,WeightType.KG);
+        GroceryItem onion= new GroceryItem(rateOfOnion, GroceryItemType.ONION);
+        Quantity quantityOfOnion= new Quantity(500,WeightType.GM);
+        cart.putItem(onion,quantityOfOnion);
+        Rate rateOfApple= new Rate(200,WeightType.KG);
+        GroceryItem apple= new GroceryItem(rateOfApple, GroceryItemType.APPLE);
+        Quantity quantityOfApple= new Quantity(1,WeightType.KG);
+        cart.putItem(apple,quantityOfApple);
+        Rate rateOfRice= new Rate(34.5,WeightType.KG);
+        GroceryItem rice= new GroceryItem(rateOfRice, GroceryItemType.RICE);
+        Quantity quantityOfRice= new Quantity(3,WeightType.KG);
+        cart.putItem(rice,quantityOfRice);
+
+        billingCounter.calculateBill(cart);
+
+        assertEquals(billingCounter.showBill(),313.5);
+
+
+    }
+
+
 
 
 
